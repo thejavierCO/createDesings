be7df2808be38be68394c89c3a109085753f338e
javier cruz <javier.cruz.odispo@hotmail.com>
2021-04-05 06:03:05 -0500

    move MakeCards

--- js/api.js
+++ js/api.js
@@ -16,36 +16,37 @@ function main(a){
     }
 }
 
+function MakeCards(data){
+    try{
+        for (let i = 0; i < data.length; i++) {
+            const ale = data[i];
+            const {id,title,imageUrl,url,publishedAt,summary,updatedAt,newsSite} = ale;
+            const baseArticle = article.querySelector("div");
+            const titleArticle = article.querySelector(".article_Title");
+            const dataArticle =article.querySelector(".article_Data");
+            // editing template
+            baseArticle.setAttribute("id",id);
+            baseArticle.style.backgroundImage = `url("${imageUrl}")`;
+            titleArticle.querySelector("span").innerText = title;
+            titleArticle.querySelector("a").setAttribute("href",url);
+            dataArticle.querySelector(".article_Description").innerText = summary;
+            dataArticle.querySelector(".article_published").innerText = moment(publishedAt)
+            dataArticle.querySelector(".article_updated").innerText = moment(updatedAt)
+            //writing template
+            const clone = article.cloneNode(true);
+            fragment.appendChild(clone);
+        }
+        articles.appendChild(fragment);
+        return articles;
+    }catch(err){
+        return err;
+    }
+}
+
 main(async ()=>{
     const fragment = document.createDocumentFragment()
     const articles = document.querySelector("div#articles")
     const article = document.querySelector("template#article").content
-    function MakeCards(data){
-        try{
-            for (let i = 0; i < data.length; i++) {
-                const ale = data[i];
-                const {id,title,imageUrl,url,publishedAt,summary,updatedAt,newsSite} = ale;
-                const baseArticle = article.querySelector("div");
-                const titleArticle = article.querySelector(".article_Title");
-                const dataArticle =article.querySelector(".article_Data");
-                // editing template
-                baseArticle.setAttribute("id",id);
-                baseArticle.style.backgroundImage = `url("${imageUrl}")`;
-                titleArticle.querySelector("span").innerText = title;
-                titleArticle.querySelector("a").setAttribute("href",url);
-                dataArticle.querySelector(".article_Description").innerText = summary;
-                dataArticle.querySelector(".article_published").innerText = moment(publishedAt)
-                dataArticle.querySelector(".article_updated").innerText = moment(updatedAt)
-                //writing template
-                const clone = article.cloneNode(true);
-                fragment.appendChild(clone);
-            }
-            articles.appendChild(fragment);
-            return articles;
-        }catch(err){
-            return err;
-        }
-    }
     let data = await getData();
     MakeCards(data);
 })
